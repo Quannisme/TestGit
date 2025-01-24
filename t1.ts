2b5d6ee0a0437218fc04c8a8bb6458c7fad42f73
Quannisme <quanismevn@gmail.com>
2025-01-24 14:13:13 +0700

    devide

--- t1.ts
+++ t1.ts
@@ -2,4 +2,7 @@ const sum = (a: number, b: number) => {
   return a + b;
 };
 
-console.log("ket qua:");
+const devide = (a: number, b: number) => {
+  return a % b;
+};
+console.log("ket qua:", devide(2, 3));
